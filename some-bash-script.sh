4919dd2b59b7359bb9f4c53ed06a418d4bb09f91
Christophe Pruvost <christophe.pruvost@oracle.com>
2022-07-06 16:50:20 +0200

    test

--- some-bash-script.sh
+++ some-bash-script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
 
+TENANCY_OCPUS=1000
+
 echo "::debug::Set the Output Variable"
-echo "::set-output name=some_output::Lookatme"
+echo "::set-output name=some_output::$TENANCY_OCPUS"
