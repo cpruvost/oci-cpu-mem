32406d230d347c25acb3866fb7546b1a043de202
Christophe Pruvost <christophe.pruvost@oracle.com>
2022-07-06 11:26:46 +0200

    test

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
-const CORE = require('@actions/core');
-// const GitHub = require('@actions/github');
-const FS = require('fs');
-const OS = require('os');
-const Util = require('util');
-const execSync = Util.promisify(require('child_process').execSync);
+const core = require("@actions/core");
+const exec = require("@actions/exec");
 
-try {
-
-  if(process.platform !== 'linux') {
-    throw new Error('This action runs only on Linux currently');
+async function run() {
+  try {
+    const someInput = core.getInput('region');
+    await exec.exec(`bash ./some-bash-script.bash ${someInput}`);
+  } catch (error) {
+    core.setFailed(error.message);
   }
-  
-  CORE.debug('Runing OCI Counter Script');
-  const output = execSync('./ociresources.bash');
-  console.log(output);
-  
-} catch (error) {
-  CORE.setFailed(error.message);
 }
+run();
---
+++ some-bash-script.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+
+echo "::set-output name=some_output::My region ($1)"
